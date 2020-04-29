328efea447d5b8423235f8846a6d7cbd0aa086f2
viniciussvieira <49722976+viniciussvieira@users.noreply.github.com>
2020-04-29 18:48:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>SGI DEMO</title>
+        <link rel="stylesheet" type="text/css"  href="style.css">
+        <script type="text/javascripit" src="script.js"></script>
+    </head>
+
+    <body>
+        
+        <div class="inicial">
+            <legend>
+                Sistema de Gestão Integrado (DEMO)
+            </legend>
+        </div>
+        <div class= "config">
+        <div class="loginuser">
+            <legend>
+                LOGIN SGI DEMO
+            </legend>
+            <span>USERNAME <input type="text" name="a" /> </span>
+        </div>
+        <div class="loginsin">
+            <span>PASSWORD <input type="password" name="b" /> </span>
+        </div>
+        <div class="button">
+            <input name="button" type="submit" value="LOGIN"/>
+            <input name="button" type="submit" value="REGISTER"/>
+        </div>
+        </div>
+    </body>
